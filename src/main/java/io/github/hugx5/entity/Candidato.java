97a414fa1e2cf11bc77e1141847dd3cc49249657
vitor hugo <rodriguesx5ti@gmail.com>
2023-08-02 13:38:09 -0300

    put

--- src/main/java/io/github/hugx5/entity/Candidato.java
+++ src/main/java/io/github/hugx5/entity/Candidato.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.util.UUID;
+
 @Entity
 @Table
 @Getter
@@ -20,4 +21,10 @@ public class Candidato {
     private String nome;
     private String foto;
     private String status;
+
+    @ManyToOne
+    @JoinColumn(name = "id_Selectiveprocess") // Nome da coluna que representa o relacionamento na tabela candidato
+    private SelectiveProcess selectiveProcess;
+
+
 }
--- src/main/java/io/github/hugx5/entity/SelectiveProcess.java
+++ src/main/java/io/github/hugx5/entity/SelectiveProcess.java
@@ -1,13 +1,17 @@
 package io.github.hugx5.entity;
 
+import io.github.hugx5.entity.Candidato;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 import java.util.UUID;
 
 @Entity
-@Table
+@Table(name = "selective_process")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -17,10 +21,12 @@ public class SelectiveProcess {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
+
     private String nome;
 
     // Relacionamento One-to-Many entre SelectiveProcess e Candidato
-    @OneToMany
-    @Column(name = "id_Selectiveprocess")
+    @OneToMany(mappedBy = "selectiveProcess", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Candidato> candidatos;
+
+    // Outros atributos e métodos da entidade...
 }
